de34df511559926a76d7338edc484c44c09fdb29
medidew <mweekes51@gmail.com>
2025-10-02 17:52:52 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/medidew/ApplicationTracker
+
+go 1.24.0
+
+toolchain go1.24.6
+
+require (
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.6 // indirect
+	golang.org/x/crypto v0.42.0 // indirect
+	golang.org/x/text v0.29.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5"
+)
+
+func main() {
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var id int
+	err = conn.QueryRow(context.Background(), "select id from test where id=$1", 1).Scan(&id)
+
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+		fmt.Printf("id: %v\n", id)
+	})
+
+	http.ListenAndServe(":3000", r)
+}
