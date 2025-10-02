46c05710298a4337aafaa02dc6f5c8a02201d73c
medidew <mweekes51@gmail.com>
2025-10-03 00:20:19 +0100

    DB auth and config.yaml

--- go.mod
+++ go.mod
@@ -10,5 +10,8 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.7.6 // indirect
 	golang.org/x/crypto v0.42.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/term v0.35.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"syscall"
+	"golang.org/x/term"
+	"gopkg.in/yaml.v3"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -13,28 +16,40 @@ import (
 	"github.com/medidew/ApplicationTracker/types"
 )
 
-func main() {
+type Config struct {
+	Database struct {
+		User string `yaml:"user`
+		Host string `yaml:"host`
+		Port string `yaml:"port`
+		Name string `yaml:"name`
+	} `yaml:"database`
+}
 
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		os.Exit(1)
+func main() {
+	database_url := os.Getenv("DATABASE_URL")
+	
+	if database_url == "" {
+		cfg, err := loadConfig("config.yaml")
+		if err != nil { errAndExit(err, 1, "failed to open config.yaml") }
+
+		fmt.Println("Enter the database password, then press enter.")
+		fmt.Println("The password will not be visible as you type it:")
+		password, err := term.ReadPassword(int(syscall.Stdin))
+		if err != nil { errAndExit(err, 1, "failed to scan password from input") }
+
+		database_url = "postgresql://" + cfg.Database.User + ":" + string(password) + "@" + cfg.Database.Host + ":" + cfg.Database.Port + "/" + cfg.Database.Name
 	}
+
+	conn, err := pgx.Connect(context.Background(), database_url)
+	if err != nil { errAndExit(err, 1, "failed to connect to db") }
 	defer conn.Close(context.Background())
 
 	var id int
 	err = conn.QueryRow(context.Background(), "select id from test where id=$1", 1).Scan(&id)
-
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		os.Exit(1)
-	}
+	if err != nil { errAndExit(err, 1, "query failed") }
 
 	test, err := types.NewJobApplication("company", types.SoftwareEngineer, types.Active)
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		os.Exit(1)
-	}
+	if err != nil { errAndExit(err, 1, "failed to create job application") }
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
@@ -45,4 +60,26 @@ func main() {
 	})
 
 	http.ListenAndServe(":3000", r)
+}
+
+func loadConfig(path string) (Config, error) {
+	cfg := Config{}
+
+	config_file, err := os.ReadFile(path)
+	if err != nil { return cfg, err }
+
+	err = yaml.Unmarshal(config_file, &cfg)
+	if err != nil { return cfg, err }
+
+	return cfg, nil
+}
+
+// Prints the given error to terminal and exits with `code`.
+func errAndExit(err error, code int, msg string) {
+	if msg != "" {
+		fmt.Printf("%v: %v\n", msg, err)
+	} else {
+		fmt.Printf("%v\n", err)
+	}
+	os.Exit(code)
 }
